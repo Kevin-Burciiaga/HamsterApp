6b6d454305db6b9534aec215087ac6872c367ac2
Kevin <kvnbecg@gmail.com>
2023-12-04 10:37:36 -0600

    Agregar jaula y recycler view animales

--- app/src/main/java/com/example/hamsterapp/Agregar_Jaula.java
+++ app/src/main/java/com/example/hamsterapp/Agregar_Jaula.java
@@ -1,14 +1,37 @@
 package com.example.hamsterapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Agregar_Jaula extends AppCompatActivity {
-
+Spinner spinner;
+String opcionSeleccionada;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_jaula);
+        spinner = findViewById(R.id.spinner);
+
+        String[] opciones = {"Conejo", "Cuyo"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, opciones);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                opcionSeleccionada = opciones[position];
+                spinner.setSelection(position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+            }
+        });
     }
 }
--- app/src/main/java/com/example/hamsterapp/Animales.java
+++ app/src/main/java/com/example/hamsterapp/Animales.java
@@ -1,8 +1,6 @@
 package com.example.hamsterapp;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -16,26 +14,19 @@ import java.util.ArrayList;
 public class Animales extends AppCompatActivity {
     RecyclerView recyclerView;
     ArrayList<Animal> arrayList = new ArrayList<>();
-    ImageView usuario, newanimal;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animales);
-        usuario = findViewById(R.id.us);
-        newanimal = findViewById(R.id.ag);
 
         recyclerView=findViewById(R.id.recyclerviewA);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         arrayList.add(new Animal(R.drawable.huella,"Hamster"));
-        arrayList.add(new Animal(R.drawable.huella,"Conejo"));
+        arrayList.add(new Animal(R.drawable.huella,"Cuyo"));
 
         AnimalesAdapter animalesRecyclerView = new AnimalesAdapter(this,arrayList);
         recyclerView.setAdapter(animalesRecyclerView);
-
-        newanimal.setOnClickListener(v -> {
-            Intent intent = new Intent(Animales.this, Agregar_Animal.class);
-            startActivity(intent);
-        });
     }
 }
