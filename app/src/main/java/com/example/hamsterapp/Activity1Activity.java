66b0b5a18ce7e290cbc1b67a52bf5937bd1a0132
Kevin <kvnbecg@gmail.com>
2023-12-11 20:46:53 -0600

    Diseño sensores, animciones, nombre y otros

--- app/src/main/java/com/example/hamsterapp/Activity1Activity.java
+++ app/src/main/java/com/example/hamsterapp/Activity1Activity.java
@@ -1,34 +1,14 @@
 package com.example.hamsterapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.os.Bundle;
 
-import java.util.ArrayList;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Activity1Activity extends AppCompatActivity {
-
-    RecyclerView recyclerView;
-    ArrayList<Model> arrayList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity1);
-
-        recyclerView=findViewById(R.id.recyclerview1);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        arrayList.add(new Model(R.drawable.temperaturaa,"TEMPERATURA"));
-        arrayList.add(new Model(R.drawable.humedad,"HUMEDAD"));
-        arrayList.add(new Model(R.drawable.velocidad,"VELOCIDAD"));
-
-        ModelRecyclerView modelRecyclerView = new ModelRecyclerView(this,arrayList);
-        recyclerView.setAdapter(modelRecyclerView);
-
-
-
     }
 }
